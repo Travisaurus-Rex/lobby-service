bea620b6c02e102b393aebdab05f4983fc83cfea
Travis Adams <brokenCD@live.com>
2026-02-07 15:49:04 +0700

    move web socket to websocket.js

--- server/server.js
+++ server/server.js
@@ -2,26 +2,13 @@ const express = require("express");
 const http = require("http");
 const app = express();
 const port = 8080;
+const setupWebsocket = require("./websocket");
 
 app.use(express.static("../client"));
 
 const server = http.createServer(app);
 
-const WebSocket = require("ws");
-const socket = new WebSocket.Server({ server });
-
-socket.on("connection", (ws) => {
-  ws.on("message", (message) => {
-    console.log(`Received: ${message.toString()}`);
-    ws.send(`Server received: ${message}`);
-  });
-
-  ws.on("close", () => {
-    console.log("Client disconencted");
-  });
-
-  ws.send("Welcome to the lobby server!");
-});
+setupWebsocket(server);
 
 server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
